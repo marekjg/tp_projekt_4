4523f6a20e89ae59dfb4b656eecd4a3935cdf580
KroliKot <84538832+KroliKot@users.noreply.github.com>
2024-06-06 19:38:48 +0200

    git problem

--- build/SDL/include/SDL_revision.h
+++ build/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/KroliKot/tp_projekt_4.git@9ca571e85af67806b4aa7a81fa19d0480f45309b"
+#define SDL_REVISION "https://github.com/KroliKot/tp_projekt_4.git@9a3ab7472a9cd72ee0d1a6ecdc9294a4c74f83a8"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
