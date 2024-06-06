053ac777ac1b465098fee19d895245cce34206f1
KroliKot <84538832+KroliKot@users.noreply.github.com>
2024-06-06 19:36:16 +0200

    poprawki

--- build/SDL/include/SDL_revision.h
+++ build/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/KroliKot/tp_projekt_4.git@ec701a6a0265c9788e783522de2ffb0c57f6d960"
+#define SDL_REVISION "https://github.com/KroliKot/tp_projekt_4.git@9ca571e85af67806b4aa7a81fa19d0480f45309b"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
